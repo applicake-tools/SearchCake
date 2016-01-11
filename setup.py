d1ea27a10996c54441c7cd00ecd2092c53df2fb6
wolski <wewolski@gmail.com>
2016-01-11 14:06:49 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="toolscake",
+    version="0.0.1",
+    author="Lorenz Blum",
+    maintainer=['Lorenz Blum', 'Witold Wolski'],
+    author_email="blum@id.ethz.ch",
+    maintainer_email=["blum@id.ethz.ch",'wewolski@gmail.com'],
+    description="doing this and that",
+    license="BSD",
+    packages=['toolscake'],
+    url='https://github.com/applicake-tools/applicake',
+    install_requires=['Unimod','applicake', 'pyteomics', 'ruffus', 'configobj']
+)
