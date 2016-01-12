cd3472f3a22c742f23cdf8635e69a4608edbd24d
wolski <wewolski@gmail.com>
2016-01-12 09:49:13 +0100

    version update

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="searchcake",
-    version="0.0.1",
+    version="0.0.3",
     author="Lorenz Blum",
     maintainer=['Lorenz Blum', 'Witold Wolski'],
     author_email="blum@id.ethz.ch",
     maintainer_email=["blum@id.ethz.ch",'wewolski@gmail.com'],
     description="tpp search workflow with peptideprophet and proteinprophet staring from mzXML files",
     license="BSD",
-    packages=['SearchCake'],
+    packages=find_packages(),
     url='https://github.com/applicake-tools/searchcake',
     install_requires=['Unimod','applicake', 'pyteomics', 'ruffus', 'configobj']
 )
